d2d9f1ab50a774b079d9b21792692d819438ec15
divyanshdubey9100 <divyanshdubey9100@gmail.com>
2025-12-27 21:13:51 +0530

    register page dob validation and custom button

--- webapp/src/app/register/register.ts
+++ webapp/src/app/register/register.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators, AbstractControl } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators, AbstractControl, FormControl } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { FormValidatorService } from '../services/form-validaton.service';
+import { DatePickerComponent } from '../date-picker/date-picker.component';
 
 @Component({
   selector: 'app-register',
@@ -9,12 +10,14 @@ import { FormValidatorService } from '../services/form-validaton.service';
   templateUrl: './register.html',
   styleUrls: ['./register.css'],
   imports: [CommonModule, 
-            ReactiveFormsModule,            
+            ReactiveFormsModule,
+            DatePickerComponent         
           ],
 })
 export class RegisterComponent implements OnInit{
  
   registerUserForm !: FormGroup
+  today = new Date().toISOString().split('T')[0];
 
   constructor(
     private formData: FormBuilder,
@@ -25,7 +28,13 @@ export class RegisterComponent implements OnInit{
       firstName: ['', [Validators.required, Validators.minLength(2), this.formValidator.getValidator('name', 'name')]],
       middleName: ['', [Validators.minLength(2), this.formValidator.getValidator('name', 'name')]],
       lastName: ['', [Validators.required, Validators.minLength(2), this.formValidator.getValidator('name', 'name')]],
-      mobile: ['', [Validators.required, this.formValidator.getValidator('mobile', 'mobile')]]
+      mobile: ['', [Validators.required, this.formValidator.getValidator('mobile', 'mobile')]],
+      dob: new FormControl<string>('', {
+          nonNullable: true,
+          validators: [this.formValidator.getDobValidator(18, 1900),
+            Validators.required,
+            this.formValidator.getValidator('dateofbirth', 'dateofbirth')],
+        }),
     })
   }
 
@@ -55,6 +64,10 @@ export class RegisterComponent implements OnInit{
     if (control.errors['maxlength']) return `${controlName} cannot exceed ${control.errors['maxlength'].requiredLength} characters`;
     if (control.errors['pattern']) return `${controlName} format is invalid`;
     return this.formValidator.getErrorMessage(control);
+  }
+
+   get dob(): FormControl<string> {
+    return this.registerUserForm.get('dob') as FormControl<string>;
   }
   
 }
